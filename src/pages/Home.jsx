ad1b9055466030de84423fdecfd0ca89e53c863e
Diurn <diurn.design@gmail.com>
2026-07-18 15:40:24 +0200

    Affichage des 20 logements en cards cliquables

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Card from '../components/Card'
 
 const Home = () => {
-  const [logements, setLogements] = useState([])
+  const [accommodation, setAccommodation] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:8080/api/properties')
       .then((response) => response.json())
-      .then((data) => setLogements(data))
+      .then((data) => setAccommodation(data))
   }, [])
 
-  console.log(logements)
-
-  return <h1>Accueil</h1>
+  return (
+    <main>
+      {accommodation.map((logement) => (
+        <Link key={logement.id} to={`/logement/${logement.id}`}>
+          <Card cover={logement.cover} title={logement.title} />
+        </Link>
+      ))}
+    </main>
+  )
 }
 
 export default Home
