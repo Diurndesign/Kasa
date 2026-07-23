24fb90a9004d490fbfc3a08fa06e890126cace45
Diurn <diurn.design@gmail.com>
2026-07-23 23:33:38 +0200

    Page d'erreur 404 créé et fonctionnelle

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom'
+import './error.scss'
+
 const Error = () => {
-  return <h1>Erreur</h1>
+  return (
+    <main className="error">
+      <h1 className="error__code">404</h1>
+      <p className="error__message">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error__link">Retourner sur la page d'accueil</Link>
+    </main>
+  )
 }
 
 export default Error
